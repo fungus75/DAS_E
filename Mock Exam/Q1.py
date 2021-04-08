3a6c5ec9e0e584fefeb70aaf8ff0767362d1ea5b
MaxPrinz <maxprinz97@web.de>
2021-04-08 09:30:55 +0200

    added code mock-exam

---
+++ Mock Exam/Q1.py
@@ -0,0 +1,5 @@
+hometown = input("My hometown is: ")
+charHometown = len(hometown)
+print("My hometown has this many letters: " + str(charHometown))
+print("The code is provided in the attached file.")
+
