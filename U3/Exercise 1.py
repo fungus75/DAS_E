afc24234db9d6bcf22ac856fa74328accef4923c
MaxPrinz <maxprinz97@web.de>
2021-04-29 11:40:12 +0200

    added analysis of ham and spam

--- U3/Exercise 1.py
+++ U3/Exercise 1.py
@@ -2,7 +2,7 @@ import os
 
 
 def loadFile(fileName):
-    return open(fileName).read()
+    return open(fileName).read().lower()
 
 
 def getFileOfDirectory(dirPath):
@@ -14,9 +14,48 @@ def countDollar(string):
     return string.count("$")
 
 
-print(countDollar(loadFile(r"spam\10.txt")))
+def amountWords(string):
+    word_list = string.split()
+    number_of_words = len(word_list)
+    return number_of_words
 
-print(loadFile(r"spam\10.txt"))
+
+def countStrangeWords(string):
+    count = 0
+    strange_word_list = ["porn", ".....", " , ", "penis", "viagra", "medicin", "rolex", "http ", " @ ", "money", "free",
+                         "price", "sperm"]
+    for word in strange_word_list:
+        count += string.count(word)
+    return count
+
+
+def printOverview(string):
+    print(string)
+    print(amountWords(string))
+    print(countDollar(string))
+
+
+def wordstatistik(dir):
+    dictionary = {}
+    fileList = getFileOfDirectory(dir)
+    for file in fileList:
+        string = loadFile(f"{dir}/{file}")
+        countWords(string, dictionary)
+    return sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+
+def countWords(string, dictionary = {}):
+    words = string.split()
+    for word in words:
+        if word in dictionary.keys():
+            dictionary[word] += 1
+        else:
+            dictionary[word] = 1
+    return dictionary
+
+
+print(wordstatistik("ham"))
+
+#print(loadFile(r"spam\10.txt"))
 
 # file = open("emails.csv", "r")
 #
