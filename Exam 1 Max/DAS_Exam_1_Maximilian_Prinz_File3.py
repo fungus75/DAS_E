ca90149dc7a3f44ea1b01ba46d6f4e98bd071979
MaxPrinz <maxprinz97@web.de>
2021-04-29 10:29:18 +0200

    added ansätze file

--- Exam 1 Max/DAS_Exam_1_Maximilian_Prinz_File3.py
+++ Exam 1 Max/DAS_Exam_1_Maximilian_Prinz_File3.py
@@ -1,12 +1,12 @@
 # DAS E Exam 15.04.2021
 # Inputs:
-# has = Sum of ASCI Codes of all characters
+# hash = Sum of ASCI Codes of all characters
 original_message = "Fabulous!"
 signature = 87236
 privateKey = (294767, 698069)
 publicKey = (971, 698069)
 
-# Generate has
+# Generate hash
 hash = 0
 for i in original_message:
     asciValue = ord(i)
