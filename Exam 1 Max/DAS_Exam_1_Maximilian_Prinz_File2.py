1ea0a5c3960c46bce592de977c498113650125a3
MaxPrinz <maxprinz97@web.de>
2021-04-29 08:44:25 +0200

    adaptations

--- Exam 1 Max/DAS_Exam_1_Maximilian_Prinz_File2.py
+++ Exam 1 Max/DAS_Exam_1_Maximilian_Prinz_File2.py
@@ -1,17 +1,20 @@
 # DAS E Exam 15.04.2021
-# Variables:
-count = 0
+# Function
+
+def counter(file, letter):
+    count = 0
+    for line in file:
+        #lower to catch all
+        line.lower()
+        for char in line:
+            if char == letter:
+                count += 1
+    return count
+
+
 # Open file
 file = open("RandomText.txt", "r")
-# read file per line
-for line in file:
-    #lower to catch all
-    line.lower()
-    for char in line:
-        if char == "p":
-            count += 1
-
-print(count)
+print(counter(file,"p"))
 
 
 
